ab303791c2361593bf513975253a14ebcbe49d02
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2021-06-09 02:54:28 -0300

    adding enum, like enum postgresql

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,7 @@ class Order < ApplicationRecord
   has_many :order_products
 
   validates :name, :phone_number, :total_value, :city, :neighborhood, :street, :number, presence: true
+
+  # Instead insert status order as 1 or 0, i'll insert a string
+  enum status: { waiting: 0, delivered: 1}
 end
