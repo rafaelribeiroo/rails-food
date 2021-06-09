a511aef3d14ab9903cbb442fdb7334e0b9242ee3
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2021-06-09 03:05:48 -0300

    end instead done, error syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
     scope '/api', defaults: {format: :json} do
+        # rails routes
         resources :categories, only: [:index]
         resources :restaurants, only: [:index, :show]
         resources :orders, only: [:create, :show]
         resources :available_cities, only: [:index]
-        # rails routes
-    done
+    end
 end
