04c7bf1e81b8a71e359a285d8e8445aed5c6005f
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2021-06-09 02:52:24 -0300

    columns required

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,6 @@
 class Category < ApplicationRecord
     # a restaurant will belongs to an category
-    belongs_to :category
+    has_many :restaurants
 
-    has_many :product_categories
-    has_many :orders
+    validates :title, presence: true
 end
